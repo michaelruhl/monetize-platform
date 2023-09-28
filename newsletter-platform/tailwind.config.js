f69b1cb5edee3a228c3caae1f1ce097d628aa68f
Michael Ruhl <michaelruhl14@hotmail.com>
2023-09-28 08:19:17 -0400

    setup newsletters page

--- newsletter-platform/tailwind.config.js
+++ newsletter-platform/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  plugins: [
+    require('flowbite/plugin')
+],
   content: [
+    "./node_modules/flowbite/**/*.js",
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
